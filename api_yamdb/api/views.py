0fcae3196fcd7adf8069bf4bada5357f4e993652
Ирина Толчеева <irinatolceeva@MacBook-Pro-Irina.local>
2022-08-10 23:01:05 +1000

    отред импорты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,18 +1,18 @@
-from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly,)
-
-from rest_framework import filters, permissions, status, viewsets
-from django.shortcuts import get_object_or_404
-from rest_framework.pagination import PageNumberPagination
-from .models import Review, Title
-from .permission import (
-    AuthorAdminModeratorObjectPermission,)
-from .serializers import (
-    CommentSerializer,
-    ReviewSerializer,)
-
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+
+from rest_framework import filters, permissions, status, viewsets
+
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,)
+from rest_framework.pagination import PageNumberPagination
+from reviews.models import Review, Title
+from reviews.permission import (
+    AuthorAdminModeratorObjectPermission,)
+from api.serializers import (
+    CommentSerializer,
+    ReviewSerializer,)
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
